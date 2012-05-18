f402c251bdc50583ceb74f232c55b06479b9b010
Tommy Marshall <tommy.marshall@viget.com>
2012-05-18 17:25:08 -0400

    Changed copy slightly

--- index.php
+++ index.php
@@ -96,7 +96,7 @@ if ($_POST) {
 			</h1>
 
 			<p>
-				Some introductory text. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
 				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
